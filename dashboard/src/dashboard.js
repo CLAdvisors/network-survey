eb0cd2932cc43f000530c6596aa1ae0469ac11e2
Bennett Garcia <bcg77@scarletmail.rutgers.edu>
2023-07-13 11:09:16 -0400

    Added animated graph to display data

--- dashboard/src/dashboard.js
+++ dashboard/src/dashboard.js
@@ -4,6 +4,7 @@ import TabularDataComponent from './TabularDataComponent.js';
 import NameStatusIcon from './NameStatusIcon';
 import QuestionStatusIcon from './QuestionStatusIcon';
 import TextInput from './TextInput';
+import Graph from './Graph';
 // TODO for MVP
 // - Redo the UI
 // - Change survey results download to csv
@@ -184,6 +185,26 @@ const Dashboard = () => {
         <h2>User Data</h2>
         <div className="row">
           <TabularDataComponent activeSurvey={activeSurvey}/>
+        </div>
+        <div className="row">
+          <Graph vertexSet={[
+        {id: '1', label: 'Vertex 1'},
+        {id: '2', label: 'Vertex 2'},
+        {id: '3', label: 'Vertex 3'},
+        {id: '4', label: 'Vertex 4'},
+        {id: '5', label: 'Vertex 5'},
+        {id: '6', label: 'Vertex 6'},
+        {id: '7', label: 'Vertex 7'},
+      ]} edgeSet={[
+        {source: '1', target: '2', label: 'Edge 1-2'},
+        {source: '1', target: '3', label: 'Edge 1-3'},
+        {source: '2', target: '4', label: 'Edge 2-4'},
+        {source: '3', target: '5', label: 'Edge 3-5'},
+        {source: '4', target: '5', label: 'Edge 4-5'},
+        {source: '6', target: '7', label: 'Edge 6-7'},
+        {source: '2', target: '7', label: 'Edge 2-7'},
+        {source: '2', target: '5', label: 'Edge 2-5'}
+      ]}></Graph>
         </div>
         <h2>Email Contents</h2>
         <div className="row">

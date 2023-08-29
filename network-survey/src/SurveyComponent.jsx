a17aac4498ce4fc923b3007197dcf902a2808667
Bennett Garcia <bcg77@scarletmail.rutgers.edu>
2023-08-29 13:51:46 -0400

    Changed endpoint on survey component

--- network-survey/src/SurveyComponent.jsx
+++ network-survey/src/SurveyComponent.jsx
@@ -14,7 +14,7 @@ function SurveyComponent({setTitle}) {
     // Get survey question json from questions api
     const [json, setJson] = React.useState(null);
     React.useEffect(() => {
-      const url = `https://network-survey-lb-1148380680.us-east-1.elb.amazonaws.com/api/questions?surveyName=${surveyName}`;
+      const url = `https://api.bennetts.work/api/questions?surveyName=${surveyName}`;
       sendRequest(url, (data) => { setJson(data.questions); setTitle(data.title); });
     }, [surveyName]);
     
@@ -27,7 +27,7 @@ function SurveyComponent({setTitle}) {
 
     survey.onComplete.add((sender, options) => {
         let data = JSON.stringify(sender.data, null, 3);
-        let url = `https://network-survey-lb-1148380680.us-east-1.elb.amazonaws.com/api/user`
+        let url = `https://api.bennetts.work/api/user`
 
         if (userId === 'demo') {
           return;
@@ -39,7 +39,7 @@ function SurveyComponent({setTitle}) {
 
     survey.onChoicesLazyLoad.add((_, options) => {
         console.log("YES IT WENT HERE")
-        const url = `https://network-survey-lb-1148380680.us-east-1.elb.amazonaws.com/api/names?skip=${options.skip}&take=${options.take}&filter=${options.filter}&surveyName=${surveyName}`;
+        const url = `https://api.bennetts.work/api/names?skip=${options.skip}&take=${options.take}&filter=${options.filter}&surveyName=${surveyName}`;
         sendRequest(url, (data) => { options.setItems(data.names, data.total); });
     });
     

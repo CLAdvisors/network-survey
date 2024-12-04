83ffb9791edcd1eb3fb990ce397e0b85e12af691
ttenneb <bcg77@scarletmail.rutgers.edu>
2024-12-04 15:02:41 -0500

    delete old folder

--- scripts/run-db-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$1" == "local" ]; then
-    liquibase --defaultsFile=liquibase.local.properties update
-elif [ "$1" == "remote" ]; then
-    liquibase --defaultsFile=liquibase.properties update
-else
-    echo "Usage: $0 [local|remote]"
-fi

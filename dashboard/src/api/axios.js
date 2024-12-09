d97219722f5de4b4d12b63ce61eeb654fb766e9d
ttenneb <bcg77@scarletmail.rutgers.edu>
2024-12-09 11:56:00 -0500

    Remove 401 forwarding

--- dashboard/src/api/axios.js
+++ dashboard/src/api/axios.js
@@ -5,6 +5,6 @@ const api = axios.create({
   baseURL: `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/api`,
   withCredentials: true  // Important! This sends cookies
 });
-console.log(`${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`)
+
 export default api;
 
